639eeb1b579e95de7636bb812186271223a0215c
Crom <doyoung.yang2005@gmail.com>
2026-07-20 00:49:42 +0900

    [Chore] #1 백엔드 프로젝트 생성

---
+++ src/test/java/yooze/withme/WithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package yooze.withme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
