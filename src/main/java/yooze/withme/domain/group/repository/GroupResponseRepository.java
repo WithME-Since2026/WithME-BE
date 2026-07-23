7147b5647f2f4571a0d1d6814d65cbbc0b1413ff
Minseong Kim <minseong.info@gmail.com>
2026-07-23 19:33:13 +0900

    [Fix] #3 GroupResponseRepository N+1 방지를 위해 fetch join 쿼리로 변경
    코드 리뷰 반영: GroupResponse.member가 LAZY라서 발생 가능한 N+1을 막기 위해
    파생 쿼리 메서드 대신 @Query + join fetch로 재작성

--- src/main/java/yooze/withme/domain/group/repository/GroupResponseRepository.java
+++ src/main/java/yooze/withme/domain/group/repository/GroupResponseRepository.java
@@ -3,11 +3,15 @@ package yooze.withme.domain.group.repository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import yooze.withme.domain.group.entity.GroupResponse;
 
 public interface GroupResponseRepository extends JpaRepository<GroupResponse, Long> {
 
-    List<GroupResponse> findByGroupRoundId(Long roundId);
+    @Query("select gr from GroupResponse gr join fetch gr.member where gr.groupRound.id = :roundId")
+    List<GroupResponse> findByGroupRoundId(@Param("roundId") Long roundId);
 
-    Optional<GroupResponse> findByGroupRoundIdAndMemberId(Long roundId, Long memberId);
+    @Query("select gr from GroupResponse gr join fetch gr.member where gr.groupRound.id = :roundId and gr.member.id = :memberId")
+    Optional<GroupResponse> findByGroupRoundIdAndMemberId(@Param("roundId") Long roundId, @Param("memberId") Long memberId);
 }
