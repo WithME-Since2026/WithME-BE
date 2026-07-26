570c4bc11cde5ac39e85973e789f76898b61790e
Crom <doyoung.yang2005@gmail.com>
2026-07-26 17:07:43 +0900

    [Fix] #8 Success코드 잘못된 거 수정 - WithME -> COMM - 문구 일부 수정

--- src/main/java/yooze/withme/common/status/SuccessStatus.java
+++ src/main/java/yooze/withme/common/status/SuccessStatus.java
@@ -9,9 +9,9 @@ import yooze.withme.common.base.BaseStatus;
 @AllArgsConstructor
 public enum SuccessStatus implements BaseStatus {
 
-    SUCCESS_200("WITHME_200", HttpStatus.OK, "성공적으로 처리했습니다."),
-    SUCCESS_201("WITHME_201", HttpStatus.CREATED, "성공적으로 생성했습니다."),
-    SUCCESS_204("WITHME_204", HttpStatus.NO_CONTENT, "성공적으로 삭제했습니다."),
+    SUCCESS_200("COMM_200", HttpStatus.OK, "요청을 성공했습니다."),
+    SUCCESS_201("COMM_201", HttpStatus.CREATED, "요청을 생성했습니다."),
+    SUCCESS_204("COMM_204", HttpStatus.NO_CONTENT, "요청을 성공했습니다."),
 
     /**
      * Group
