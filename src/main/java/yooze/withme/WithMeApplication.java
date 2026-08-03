3036c3cd0e0989302df65ec6d2271dfe3a81933a
Crom <doyoung.yang2005@gmail.com>
2026-08-03 20:56:48 +0900

    [Feat] #16 webhook 설정 추가

--- src/main/java/yooze/withme/WithMeApplication.java
+++ src/main/java/yooze/withme/WithMeApplication.java
@@ -3,10 +3,11 @@ package yooze.withme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import yooze.withme.common.properties.DiscordWebhookProperties;
 import yooze.withme.common.properties.JwtProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JwtProperties.class)
+@EnableConfigurationProperties({JwtProperties.class, DiscordWebhookProperties.class})
 public class WithMeApplication {
 
     public static void main(String[] args) {
