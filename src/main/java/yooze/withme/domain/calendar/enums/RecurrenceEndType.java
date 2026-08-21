9b3bbca450c44dcc2fef0bdbd64bf56548c94d4b
Crom <doyoung.yang2005@gmail.com>
2026-08-20 22:28:37 -0700

    [Feat] #30 반복 일정 enum추가

---
+++ src/main/java/yooze/withme/domain/calendar/enums/RecurrenceEndType.java
@@ -0,0 +1,7 @@
+package yooze.withme.domain.calendar.enums;
+
+public enum RecurrenceEndType {
+    NEVER,
+    DATE,
+    COUNT
+}
---
+++ src/main/java/yooze/withme/domain/calendar/enums/RecurrenceOwnerType.java
@@ -0,0 +1,6 @@
+package yooze.withme.domain.calendar.enums;
+
+public enum RecurrenceOwnerType {
+    TODO,
+    SCHEDULE
+}
