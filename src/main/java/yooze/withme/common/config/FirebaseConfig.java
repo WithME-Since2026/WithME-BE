763095cc41fa6f391d3d173b1e2713455bde4dd9
hesseo <heeseo3670@naver.com>
2026-09-06 16:47:04 +0900

    [Fix] #31 테스트 환경 Firebase 설정 누락 및 컨텍스트 로딩 실패 수정

--- src/main/java/yooze/withme/common/config/FirebaseConfig.java
+++ src/main/java/yooze/withme/common/config/FirebaseConfig.java
@@ -14,6 +14,7 @@ import java.io.InputStream;
 
 @Slf4j
 @Configuration
+@org.springframework.context.annotation.Profile("!test")
 public class FirebaseConfig {
 
     @Value("${firebase.service-account-key-path}")
