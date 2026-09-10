b7ecc8bd9911240968b55767778d9bf95bf27a14
Crom <doyoung.yang2005@gmail.com>
2026-09-10 12:07:56 -0700

    [Fix] #30 HOLYDAY_API 500에러 수정

--- src/test/java/yooze/withme/domain/calendar/service/HolidayApiClientTest.java
+++ src/test/java/yooze/withme/domain/calendar/service/HolidayApiClientTest.java
@@ -3,11 +3,11 @@ package yooze.withme.domain.calendar.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
 import yooze.withme.domain.calendar.entity.Holiday;
 import yooze.withme.domain.calendar.enums.HolidayType;
 
